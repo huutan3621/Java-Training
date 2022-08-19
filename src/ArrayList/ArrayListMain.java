470da6637f60e5c3488831b1e7ea425f3b9ef804
huutan3621 <huutan3621@gmail.com>
2022-08-19 14:16:44 +0700

    arraylist

---
+++ src/ArrayList/ArrayListMain.java
@@ -0,0 +1,14 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class ArrayListMain {
+    public static void main(String[] args) {
+        ArrayList<String> students = new ArrayList<String>();
+        students.add("Tan Vuu");
+        students.add("Tan Vo");
+        students.add("Yen Hoang");
+        students.add("Dat Pham");
+        System.out.println(students);
+    }
+}
